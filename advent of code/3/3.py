569e8138e56bc23a9ce64ca57c3dcd9158e46629
omer.shaashua1 <omer.shaashua1@gmail.com>
2024-12-07 15:39:10 +0200

    finished

--- advent of code/3/3.py
+++ advent of code/3/3.py
@@ -1,6 +1,27 @@
 from pathlib import Path
-
+import re
 
 input = Path("G:\My Drive\Python Projects\input3.txt").read_text()
 
 
+
+pattern = r"(mul\(\d+,\d+\)|don't\(\)|do\(\))"
+
+ops = re.findall(pattern, input)
+
+
+commands_active = True
+
+sum_of_mults = 0
+for op in ops:
+    if op == "do()":
+        commands_active = True
+    elif op == "don't()":
+        commands_active = False
+    else:
+        if commands_active:
+            op = op[4:-1]
+            pair = op.split(",")
+            mult = int(pair[0]) * int(pair[1])
+            sum_of_mults = sum_of_mults + mult
+print(sum_of_mults)
