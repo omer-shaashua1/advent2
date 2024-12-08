ca0676174868b9e04120e708638b797062531ed8
omer.shaashua1 <omer.shaashua1@gmail.com>
2024-12-08 17:47:29 +0200

    dumbed down the code, still doesn't work

--- advent of code/2/advent_2_2.py
+++ advent of code/2/advent_2_2.py
@@ -1,5 +1,8 @@
-input_reports = list(open("input2.txt", "r"))
+from pathlib import Path
 
+
+# input_reports = open("input2.txt", "r").split()
+input_reports = Path("G:\My Drive\Python Projects\input2.txt").read_text().split("\n")
 
 def check_level_diff(report: list):
     for i in range(len(report)-1):
@@ -7,114 +10,18 @@ def check_level_diff(report: list):
         next_level = int(report[i+1])
         if 0 == curr_level-next_level or abs(curr_level-next_level) >= 4 :
             return(False)
-        return(True)
-
-
-def report_step_checker(report: list):
-    for i in range(len(report)-1):
-        curr_level = int(report[i])
-        next_level = int(report[i+1])
-        if 0 == curr_level-next_level or abs(curr_level-next_level) >= 4:
-            return(i)
     return(True)
 
 
-def step_fixer_idx(report: list, idx: int):
-    new_report = report.copy()
-    new_report.pop(idx)
-    if report_step_checker(new_report):
-        return(new_report)
-    return(False)
-    
-
-def step_fixed_after_idx(report: list, idx: int):
-    new_report = report.copy()
-    new_report.pop(idx+1)
-    if report_step_checker(new_report):
-        return(new_report)
-    return(False)
+def can_fix(report: list):
+    for i in range(len(report)):
+        fixed_report = report.copy()
+        fixed_report.pop(i)
+        if monotonic(fixed_report) and check_level_diff(fixed_report):
+            return True
+    return False
 
 
-def mon_inc(report: list):
-    report_couples = zip(report, report[1:])
-    for x,y in report_couples:
-        if x > y:
-            return report.index(x)
-    return(True)
-
-
-def mon_dec(report: list):
-    report_couples = zip(report, report[1:])
-    for x,y in report_couples:
-        if x < y:
-            return report.index(x)
-    return(True)
-
-
-def monotonicity_fixer_idx(report: list, idx: int):
-    new_report = report.copy()
-    new_report.pop(idx)
-    if monotonic(new_report):
-        return(new_report)
-    return(False)
-
-
-def monotonicity_fixer_after_idx(report: list, idx:int):
-    new_report = report.copy()
-    new_report.pop(idx+1)
-    if monotonic(new_report):
-        return(new_report)
-    return(False)
-
-
-def check_direction(report: list):
-    step_inc = 0
-    step_dec = 0
-    report_couples = zip(report, report[1:])
-    for x,y in report_couples:
-        if x < y:
-            step_inc = step_inc + 1
-        if x > y: 
-            step_dec = step_dec + 1
-        if step_dec == 1:
-            return("inc")
-        if step_inc == 1:
-            return("dec")
-    return(False)
-
-
-
-    
-# for j in range(len(input_reports)):
-#     split_level = input_reports[j].split()
-#     if report_step_checker(split_level):
-#         if monotonic(split_level):
-#             safe_levels = safe_levels + 1
-#         edited_report = monotonicity_fixer_idx(split_level, mon_inc)
-#         if edited_report
-
-
-
-
-# def report_fixer_monotonic(report: list):
-#     report_couples = zip(report, report[1:])
-#     for x,y in report_couples:
-#         if x > y:
-#             new_report = report
-#             new_report.remove(x)
-#             new_report_couples = zip(new_report, new_report[1:])
-#             for x,y in new_report_couples:
-#                 if x > y:
-#                     new_report = report
-#                     new_report.remove(y)
-#                     new_report_couples = zip(new_report, new_report[1:])
-#                     for x,y in new_report_couples:
-#                         if x > y:
-#                             return(False)
-#                     return(True)
-#                 return(True)
-#             return (new_report)
-#     return 3
 
 def check_inc(list: list):
     return all(i < j for i, j in zip(list,list[1:]))
@@ -129,24 +36,14 @@ def monotonic(list: list):
 safe_levels=0
 
 
+for report in input_reports:
+    split_report = report.split()
+    if monotonic(split_report) and check_level_diff(split_report):
+        safe_levels = safe_levels + 1
+    else:
+        if can_fix(split_report):
+            safe_levels = safe_levels + 1
+    
 
-# for j in range(len(input_reports)):
-#     split_level = input_reports[j].split()
-#     if report_step_checker(split_level):
-#         if monotonic(split_level):
-#             safe_levels = safe_levels + 1
-#         edited_report = monotonicity_fixer_idx(split_level, mon_inc)
-#         if edited_report
+print(safe_levels)
 
-# for j in range(len(input_reports)):
-#     split_level = input_reports[j].split()
-#     if report_fixer_step(split_level):
-#         safe_levels = safe_levels + 1
-
-test_report = [1, 2, 3, 5, 7, 2]
-test_idx = report_step_checker(test_report)
-
-#print(step_fixer(test_report, test_idx))
-
-# if "int"=="int":
-#     print(1)
