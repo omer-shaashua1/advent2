ae19def59fa4fce20f697fb3e13e8a289914a5a8
omer.shaashua1 <omer.shaashua1@gmail.com>
2024-12-10 00:45:00 +0200

    created the starting point for the level

--- advent of code/6/6.py
+++ advent of code/6/6.py
@@ -3,4 +3,16 @@ from pathlib import Path
 map_input = Path("G:\My Drive\Python Projects\input6.txt").read_text().split()
 
 
-print(map_input)
+map_start = []
+
+
+for i in range(len(map_input)):
+    row = []
+    for j in range(len(map_input[i])):
+        row.append(map_input[i][j])
+    map_start.append(row)
+
+
+
+
+print(map_start)
