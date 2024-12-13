4a1f085095dafd1cdab168ab6b0074e4de071c2a
Omer Shaashua <omer.shaashua1@gmail.com>
2024-12-13 19:21:47 +0200

    finished

--- advent of code/9/9.py
+++ advent of code/9/9.py
@@ -1,31 +1,34 @@
 from pathlib import Path
 
-string_input = Path("G:\My Drive\Python Projects\input9.txt").read_text()
+string_input = Path(r"C:\Users\user 1\Downloads\python\advent2\input9.txt").read_text()
 # string_input = "2333133121414131402"
-input_with_dots = ''
 
+
+input_with_dots = []
 
 for i in range(len(string_input)):
     if i % 2 == 0:
-        input_with_dots = input_with_dots + str(int(i/2)) * int(string_input[i])
+        list_to_add = [str(int(i/2))] * int(string_input[i])
+        input_with_dots += list_to_add
     if i % 2 == 1:
-        input_with_dots = input_with_dots + "." * int(string_input[i])
+        list_to_add = ["."] * int(string_input[i])
+        input_with_dots += list_to_add
 
-input_list = list(input_with_dots)
 
 i, j = 0, len(input_with_dots)-1
 while i <= j:
-    while i < len(input_list) and input_list[i] != ".":
-        i += 1
-    while input_list[j] == '.':
-        j -= 1
+    while i < len(input_with_dots) and input_with_dots[i] != ".":
+        i = i + 1
+    while input_with_dots[j] == '.':
+        j = j - 1
     if i <= j:
-        input_list[i], input_list[j] = input_list[j], input_list[i]
+        input_with_dots[i], input_with_dots[j] = input_with_dots[j], input_with_dots[i]
 
 
-sum_of_org = 0
-for idx, char in enumerate(input_list):
+
+checksum = 0
+for idx, char in enumerate(input_with_dots):
     if char.isnumeric():
-        sum_of_org += idx * int(char)
+        checksum = checksum + (idx * int(char))
 
-print(sum_of_org)
+print("checksum =", checksum)
